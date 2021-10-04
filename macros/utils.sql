4a14cd6060031b91b21bc113c54b0eb2336bbc62
Ojasvi Harsola <ojasvi@blotout.io>
2021-10-04 18:06:53 +0530

    condition check before call statement

--- macros/utils.sql
+++ macros/utils.sql
@@ -3,7 +3,7 @@
 {% endmacro %}
 
 {% macro google_ads_unified() %}
-    {% call statement() -%}
+
         {%- set source_relation = adapter.get_relation(database = env_var('DATABASE'), schema = env_var('SCHEMA'), identifier = 'connection_pipeline') -%}
         {%- if source_relation != none %}
             {%- set get_active_pipelines %}
@@ -29,52 +29,56 @@
             {% else %}
                 {%- set source_name_list = [] -%}
             {% endif -%}
-            {%- set schema_exists = [] -%}
-            {%- for source_name in source_name_list %}
-                {%- set source_schema_name = source_name + '_'  + env_var('ENV') -%}
-                {%- set check_relation = adapter.get_relation(
-                     database = env_var('DATABASE'),
-                     schema = source_schema_name,
-                     identifier = "ad_group_ad_report")
-                -%}
-                {% if check_relation != None %}
-                     {%- set schema_exists = schema_exists.append(source_schema_name) -%}
-                {% endif -%}
-            {% endfor -%}
-            {%- for sch in schema_exists %}
-                {%- if loop.first %}
-                   CREATE TABLE
-                    {{ env_var('SCHEMA') }}.google_ads AS
-                    SELECT l.*, r.google_ads_click_id FROM (SELECT
-                        DISTINCT *
-                    FROM (
-                {% endif -%}
-                SELECT
-                     "campaign.id" as google_ads_campaign_id,
-                     "ad_group.name" as google_ads_group_name,
-                     "campaign.name" as google_ads_campaign_name,
-                     '{{ sch }}' as google_ads_provider_type
-                 FROM
-                     "{{ sch }}"."ad_group_ad_report"
-                 {%- if not loop.last %} UNION {% else %} ))l {% endif -%}
-            {% endfor -%}
 
-            {%- for sch in schema_exists %}
-                {%- if loop.first %}
-                    JOIN (SELECT
-                        DISTINCT *
-                    FROM (
-                {% endif -%}
-                SELECT
-                     "click_view.gclid" as google_ads_click_id,
-                     "campaign.id" as google_ads_campaign_id
-                 FROM
-                     "{{ sch }}"."click_view"
-                 {%- if not loop.last %} UNION {% else %} ))r ON
-                 l.google_ads_campaign_id = r.google_ads_campaign_id {% endif -%}
-            {% endfor -%}
+            {%- if source_name_list|length > 0 %}
+                {% call statement() -%}
+                    {%- set schema_exists = [] -%}
+                    {%- for source_name in source_name_list %}
+                        {%- set source_schema_name = source_name + '_'  + env_var('ENV') -%}
+                        {%- set check_relation = adapter.get_relation(
+                             database = env_var('DATABASE'),
+                             schema = source_schema_name,
+                             identifier = "ad_group_ad_report")
+                        -%}
+                        {% if check_relation != None %}
+                             {%- set schema_exists = schema_exists.append(source_schema_name) -%}
+                        {% endif -%}
+                    {% endfor -%}
+                    {%- for sch in schema_exists %}
+                        {%- if loop.first %}
+                           CREATE TABLE
+                            {{ env_var('SCHEMA') }}.google_ads AS
+                            SELECT l.*, r.google_ads_click_id FROM (SELECT
+                                DISTINCT *
+                            FROM (
+                        {% endif -%}
+                        SELECT
+                             "campaign.id" as google_ads_campaign_id,
+                             "ad_group.name" as google_ads_group_name,
+                             "campaign.name" as google_ads_campaign_name,
+                             '{{ sch }}' as google_ads_provider_type
+                         FROM
+                             "{{ sch }}"."ad_group_ad_report"
+                         {%- if not loop.last %} UNION {% else %} ))l {% endif -%}
+                    {% endfor -%}
+
+                    {%- for sch in schema_exists %}
+                        {%- if loop.first %}
+                            JOIN (SELECT
+                                DISTINCT *
+                            FROM (
+                        {% endif -%}
+                        SELECT
+                             "click_view.gclid" as google_ads_click_id,
+                             "campaign.id" as google_ads_campaign_id
+                         FROM
+                             "{{ sch }}"."click_view"
+                         {%- if not loop.last %} UNION {% else %} ))r ON
+                         l.google_ads_campaign_id = r.google_ads_campaign_id {% endif -%}
+                    {% endfor -%}
+                {%- endcall %}
+            {% endif -%}
         {% endif -%}
-    {%- endcall %}
 {% endmacro %}
 
 {% macro camel_to_snake(s) -%}
